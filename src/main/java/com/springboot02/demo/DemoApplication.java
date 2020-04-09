b189fd2854c1e00de434a340b9714c4b519976e1
847729600@qq.com <847729600@qq.com>
2020-04-09 22:10:40 +0800

    first commit

--- src/main/java/com/springboot02/demo/DemoApplication.java
+++ src/main/java/com/springboot02/demo/DemoApplication.java
@@ -2,12 +2,22 @@ package com.springboot02.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@RequestMapping("/hello")
+	public String hello() {
+		return "hello world . . ";
+	}
 
 }
